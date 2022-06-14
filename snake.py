3184d098d11f7b1c070f1f050976acb691ddc691
StepanTWL <h3xp3ctb@gmail.com>
2022-06-14 22:59:17 +0300

    start

--- snake.py
+++ snake.py
@@ -1,6 +1,31 @@
-from pickle import FALSE
 import pygame
 import sys
+import random
+
+
+class Apple():
+
+    def __init__(self, screen):
+        """inic apple"""
+
+        self.screen = screen
+        self.image = pygame.image.load('images/apple.png')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.centery = self.screen_rect.centery
+        self.newdata = False
+
+    def output(self):
+        """draw"""
+        self.rect.centerx = random.uniform(8, 792)
+        self.rect.centery = random.uniform(8, 592)
+        self.screen.blit(self.image, self.rect)
+
+    def update_apple(self):
+        """"""
+        if (self.newdata == False)
+
 
 class Snake():
 
@@ -13,6 +38,8 @@ class Snake():
         self.screen_rect = screen.get_rect()
         self.rect.centerx = self.screen_rect.centerx
         self.rect.centery = self.screen_rect.centery
+        self.centerx = float(self.rect.centerx)
+        self.centery = float(self.rect.centery)
         self.mright = False
         self.mleft = False
         self.mup = False
@@ -24,14 +51,18 @@ class Snake():
 
     def update_snake(self):
         """"""
-        if self.mright:
-            self.rect.centerx += 1
-        elif self.mleft:
-            self.rect.centerx -= 1
-        elif self.mup:
-            self.rect.centery -= 1
-        elif self.mdown:
-            self.rect.centery += 1
+        if self.mright and self.rect.right < self.screen_rect.right:
+            self.centerx += 0.1
+        elif self.mleft and self.rect.left > 0:
+            self.centerx -= 0.1
+        elif self.mup and self.rect.top > 0:
+            self.centery -= 0.1
+        elif self.mdown and self.rect.bottom < self.screen_rect.bottom:
+            self.centery += 0.1
+
+        self.rect.centerx = self.centerx
+        self.rect.centery = self.centery
+        
 
 
 
@@ -41,6 +72,8 @@ pygame.display.set_caption("Snape")
 
 bg_color = (0, 0, 0)
 snake = Snake(screen)
+apple = Apple(screen)
+apple.output()
 
 while True:
     for event in pygame.event.get():
@@ -71,7 +104,8 @@ while True:
     screen.fill(bg_color)
     snake.update_snake()
     snake.output()
+    if (apple.newdata != True): 
+        apple.output()
+        apple.newdata = True
     pygame.display.flip()
-
-
 
