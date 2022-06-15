9f8ebdb373325baee827aa20e9962f710cd905ed
StepanTWL <h3xp3ctb@gmail.com>
2022-06-15 23:07:48 +0300

    Start

--- snake.py
+++ snake.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 import random
+from pygame.sprite import Group
 
 
 class Apple():
@@ -12,38 +13,43 @@ class Apple():
         self.image = pygame.image.load('images/apple.png')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
-        self.rect.centerx = self.screen_rect.centerx
-        self.rect.centery = self.screen_rect.centery
+        self.rect.centerx = int(random.uniform(0, 49)) * 16 + 8
+        self.rect.centery = int(random.uniform(0, 39)) * 16 + 8
         self.newdata = False
 
     def output(self):
         """draw"""
-        self.rect.centerx = random.uniform(8, 792)
-        self.rect.centery = random.uniform(8, 592)
         self.screen.blit(self.image, self.rect)
 
     def update_apple(self):
         """"""
-        if (self.newdata == False)
+        if (self.newdata):
+            self.rect.centerx = int(random.uniform(0, 49)) * 16 + 8
+            self.rect.centery = int(random.uniform(0, 39)) * 16 + 8
 
 
-class Snake():
+
+class Snake(pygame.sprite.Sprite):
 
     def __init__(self, screen):
         """inic snake"""
-
+        super(Snake, self).__init__()
         self.screen = screen
         self.image = pygame.image.load('images/snake_cell.png')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
-        self.rect.centerx = self.screen_rect.centerx
-        self.rect.centery = self.screen_rect.centery
+        self.rect.centerx = self.screen_rect.centerx + 8
+        self.rect.centery = self.screen_rect.centery + 8
         self.centerx = float(self.rect.centerx)
         self.centery = float(self.rect.centery)
         self.mright = False
         self.mleft = False
         self.mup = False
         self.mdown = False
+        self.count_right = 0
+        self.count_left = 0
+        self.count_up = 0
+        self.count_down = 0
 
     def output(self):
         """draw"""
@@ -52,60 +58,90 @@ class Snake():
     def update_snake(self):
         """"""
         if self.mright and self.rect.right < self.screen_rect.right:
-            self.centerx += 0.1
+            self.count_right += 1
+            self.count_left = 0
+            self.count_up = 0
+            self.count_down = 0
+            if self.count_right == 160:
+                self.centerx += 16
+                self.count_right = 0
         elif self.mleft and self.rect.left > 0:
-            self.centerx -= 0.1
+            self.count_right = 0
+            self.count_left += 1
+            self.count_up = 0
+            self.count_down = 0
+            if self.count_left == 160:
+                self.centerx -= 16
+                self.count_left = 0
         elif self.mup and self.rect.top > 0:
-            self.centery -= 0.1
+            self.count_right = 0
+            self.count_left = 0
+            self.count_up += 1
+            self.count_down = 0
+            if self.count_up == 160:
+                self.centery -= 16
+                self.count_up = 0
         elif self.mdown and self.rect.bottom < self.screen_rect.bottom:
-            self.centery += 0.1
+            self.count_right = 0
+            self.count_left = 0
+            self.count_up = 0
+            self.count_down += 1
+            if self.count_down == 160:
+                self.centery += 16
+                self.count_down = 0
 
         self.rect.centerx = self.centerx
         self.rect.centery = self.centery
-        
+
+    def add_new_cell(self):        
 
 
 
 pygame.init()
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((800, 640))
 pygame.display.set_caption("Snape")
+cells = Group()
 
 bg_color = (0, 0, 0)
 snake = Snake(screen)
 apple = Apple(screen)
-apple.output()
 
 while True:
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_DOWN:
+            if event.key == pygame.K_DOWN and snake.mup == False:
                 snake.mdown = True
                 snake.mup = False
                 snake.mleft = False
                 snake.mright = False
-            if event.key == pygame.K_UP:
+            if event.key == pygame.K_UP and snake.mdown == False:
                 snake.mdown = False
                 snake.mup = True
                 snake.mleft = False
                 snake.mright = False
-            if event.key == pygame.K_LEFT:
+            if event.key == pygame.K_LEFT and snake.mright == False:
                 snake.mdown = False
                 snake.mup = False
                 snake.mleft = True
                 snake.mright = False
-            if event.key == pygame.K_RIGHT:
+            if event.key == pygame.K_RIGHT and snake.mleft == False:
                 snake.mdown = False
                 snake.mup = False
                 snake.mleft = False
                 snake.mright = True
 
+    if (pygame.sprite.groupcollide(snake, apple, True, False))
+
     screen.fill(bg_color)
     snake.update_snake()
     snake.output()
-    if (apple.newdata != True): 
-        apple.output()
-        apple.newdata = True
+    apple.output()
+    snake.new_snake_cell(snake, apple)
+    if (apple.newdata): 
+        apple.update_apple()
+        apple.newdata = False
     pygame.display.flip()
 
